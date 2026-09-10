0119a2b1bafdf699fc64ab934e6c172c43b2b4ae
ishaana10 <46734090+ishaana10@users.noreply.github.com>
2026-09-10 02:24:46 +0000

    Update login background, replace green logo with nuvisAccountX logo, and update serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -1,49 +1,15 @@
-var staticCacheName = "pwa-v" + new Date().getTime();
-var filesToCache = [
-    'public/img/pwa/icon-192x192.png',
-    'public/img/pwa/icon-512x512.png'
-];
-
-/*
-// Cache on install
-self.addEventListener("install", event => {
-    this.skipWaiting();
-    event.waitUntil(
-        caches.open(staticCacheName)
-            .then(cache => {
-                return cache.addAll(filesToCache);
-            })
-    )
+self.addEventListener('install', event => {
+    self.skipWaiting();
 });
-*/
-/*
-// Clear cache on activate
+
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames
-                    .filter(cacheName => (cacheName.startsWith("pwa-")))
-                    .filter(cacheName => (cacheName !== staticCacheName))
-                    .map(cacheName => caches.delete(cacheName))
+                cacheNames.map(cacheName => caches.delete(cacheName))
             );
+        }).then(() => {
+            return self.registration.unregister();
         })
-    );
-});*/
-
-// Serve from Cache
-self.addEventListener("fetch", event => {
-    if (event.request.method !== 'GET') {
-        return;
-    }
-
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-            .catch(() => {
-                return fetch(event.request);
-            })
     );
 });
